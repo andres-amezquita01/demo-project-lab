6353355500c24f26871120d1d54faaa08cce4548
andres-amezquita01 <andresfelipe.amezquita01@gmail.com>
2023-05-17 13:54:28 -0500

    Add deployment to production

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 282335569253.dkr.ecr.us-east-1.amazonaws.com
 ecr_url="282335569253.dkr.ecr.us-east-1.amazonaws.com/final-demo:latest"
+
 docker stop $(docker ps -a -q)  
 docker rmi -f $(docker images -a -q)   
 docker pull $ecr_url   
-docker run -d -p 8090:8080 $ecr_url 
+docker run -d -p 8090:8080 $ecr_url
