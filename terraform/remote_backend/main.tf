7371e86f5d9b2a1f9de65ba4f603a125574d8676
andres-amezquita01 <andresfelipe.amezquita01@gmail.com>
2023-05-23 14:14:45 -0500

    Add s3 backend to terraform

---
+++ terraform/remote_backend/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  backend "s3" {
+    bucket = "devops-project-lab"
+    key = "tf-infra/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "devops-project-lab"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
