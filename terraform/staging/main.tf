b7035d21421a1860d8d451c624fe5a63b3116604
andres-amezquita01 <andresfelipe.amezquita01@gmail.com>
2023-06-03 11:46:47 -0500

    Fix segurity group problem and now it is working perfectly!

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -18,32 +18,32 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.go_app_staging_bucket
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-     sse_algorithm = "AES256" 
-    }
-  }
-}
-resource "aws_dynamodb_table" "terraform_locks" {
-  name = var.dynamodb_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = var.go_app_staging_bucket
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
+# resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+# resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#      sse_algorithm = "AES256" 
+#     }
+#   }
+# }
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name = var.dynamodb_table
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
